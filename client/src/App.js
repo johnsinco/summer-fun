37a193e24f5b3aa59f3652aa89ec4b430ae43b8f
johnsinco <johnstewartco@outlook.com>
2018-07-11 15:47:16 -0600

    list players by name

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = {players: []}
+
+  componentDidMount() {
+    fetch('/players')
+      .then(res => res.json())
+      .then(players => this.setState({ players }));
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">All Players</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          {this.state.players.map(player =>
+            <div key={player.id}>{player.firstName} {player.lastName}</div>
+          )}
       </div>
     );
   }
