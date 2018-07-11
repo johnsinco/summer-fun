46e7213c84b0fd5b9f27953be79a429308d86293
johnsinco <johnstewartco@outlook.com>
2018-07-11 13:02:47 -0600

    more API routes

--- routes/players.js
+++ routes/players.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET players listing. */
 router.get('/', function(req, res, next) {
   res.json([
     {id: 1, firstName: "Jane", lastName: "Doe", accountId: 7},
     {id: 2, firstName: "Bill", lastName: "Baggins", accountId: 12}
   ]);
+});
+
+router.post('/', function(req, res, next) {
+  res.status(201).send('created')
 });
 
 router.get('/:id', function(req, res, next) {
@@ -15,11 +19,15 @@ router.get('/:id', function(req, res, next) {
   })
 })
 
-router.get('/:id/challenges', function(req, res, next) {
-  res.json([
-    {id: 1, name: 'Read 10 books', status: 'in progress'},
-    {id: 2, name: 'Hike a Fourteener', status: 'complete'}
-  ])
-}))
+router.route('/:id/challenges')
+  .get(function(req, res, next) {
+    res.json([
+      {id: 1, name: 'Read 10 books', status: 'in progress'},
+      {id: 2, name: 'Hike a Fourteener', status: 'complete'}
+    ])
+  })
+  .post(function(req, res, next) {
+    res.send('created')
+  })
 
 module.exports = router;
