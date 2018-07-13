50f025ebf447626d66a6aad2d2dbb449b469b93a
johnsinco <johnstewartco@outlook.com>
2018-07-13 09:38:30 -0600

    player detail start

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter,  Route } from 'react-router-dom'
 import Players from './Players'
 import Home from './Home'
 
--- client/src/Players.js
+++ client/src/Players.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom'
 
 class Players extends Component {
 
@@ -12,15 +13,47 @@ class Players extends Component {
 
   render() {
     return (
-      <div className="players">
-        <header className="players-header">
-          <h3 className="players-title">All Players</h3>
-        </header>
-          {this.state.players.map(player =>
-            <div key={player.id}>{player.firstName} {player.lastName}</div>
-          )}
-      </div>
+      <Switch>
+        <Route exact path='/players' render={(props) => (
+          <div className="players">
+            <header className="players-header">
+              <h3 className="players-title">All Players</h3>
+            </header>
+              {this.state.players.map(player =>
+                <div key={player.id}>{player.firstName} {player.lastName}</div>
+              )}
+          </div>
+        )}/>
+        <Route path='/players/:id' render={(props) => (
+          <div className="players">
+            <header className="players-header">
+              <h3 className="players-title">Player Detail</h3>
+            </header>
+            <div>
+              {this.showPlayer(props.match.params.id)}
+            </div>
+          </div>
+        )}/>
+      </Switch>
+
     );
+  }
+
+  showPlayer(id) {
+    var player = this.state.players.find(p => (parseInt(id) === p.id))
+    if(player === undefined) {
+      return(
+        <div>Player Not Found</div>
+      )
+    }
+    console.log('000000000='+id)
+    console.log(player)
+    console.log('11111')
+    return(
+      <div className="playerDetail">
+        {player.firstName} {player.lastName}
+      </div>
+    )
   }
 }
 
