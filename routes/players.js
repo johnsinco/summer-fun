d8b3735496d5fad450977455132990c818b4aaa0
johnsinco <johnstewartco@outlook.com>
2018-07-16 15:04:04 -0600

    use DB for list view

--- routes/players.js
+++ routes/players.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var router = express.Router();
+const Player = require('../server/models').Player;
 
 /* GET players listing. */
 router.get('/', function(req, res, next) {
-  res.json([
-    {id: 1, firstName: "Jane", lastName: "Doe", accountId: 7},
-    {id: 2, firstName: "Bill", lastName: "Baggins", accountId: 12}
-  ]);
+  return Player
+    .all()
+    .then(players => res.json(players))
+    .catch(err => res.status(500).send(err))
+  // res.json([
+  //   {id: 1, firstName: "Jane", lastName: "Doe", accountId: 7},
+  //   {id: 2, firstName: "Bill", lastName: "Baggins", accountId: 12}
+  // ]);
 });
 
 router.post('/', function(req, res, next) {
